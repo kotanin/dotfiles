cf1bef9a01dd2b345ce105762c0461c20e097e00
Masakatsu Kodani <kotanin.d7th@gmail.com>
2019-09-21 23:24:47 +0900

    add emacs.d.

---
+++ emacs.d/init.el
@@ -0,0 +1,39 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(defun add-to-load-path (&rest paths)
+  (let (path)
+    (dolist (path paths paths)
+      (let ((default-directory
+              (expand-file-name (concat user-emacs-directory path))))
+        (add-to-list 'load-path default-directory)
+        (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
+            (normal-top-level-add-subdirs-to-load-path))))))
+
+(add-to-load-path "elisp" "elpa" "conf" "public_repos")
+
+;; path from shell
+(exec-path-from-shell-initialize)
+
+;; setup init-loader
+(require 'init-loader)
+(init-loader-load "~/.emacs.d/inits")
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+	(tide typescript-mode json-mode visual-regexp magit go-mode git-gutter exec-path-from-shell))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
