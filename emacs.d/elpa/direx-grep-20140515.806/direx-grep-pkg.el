fb24167659a514f40b5a9e02dc92b4b4e957629e
Masakatsu Kodani <kotanin.d7th@gmail.com>
2020-01-31 02:27:52 +0900

    init.elを掃除

--- emacs.d/elpa/direx-grep-20140515.806/direx-grep-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "direx-grep" "20140515.806" "Grep node of direx.el using incremental search like anything.el/helm.el" '((direx "0.1alpha")) :commit "1109a512a80b2673a70b18b8568514049017faad" :keywords '("convenience") :url "https://github.com/aki2o/direx-grep")
